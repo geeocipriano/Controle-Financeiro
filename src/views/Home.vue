3f42e6cd2175fcb4aa694daaa5c191de61c1efdc
Geovany Cipriano <geocipriano28@gmail.com>
2021-05-19 10:28:38 -0300

    Update Home.vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -21,6 +21,7 @@
               </v-card-subtitle>
           </v-card>
         </v-col>
+        <!-- Inicio Lista de Grupos -->
         <v-col
         cols="6"
         >
@@ -31,23 +32,22 @@
             <v-list two-line>
               <v-list-item-group
               >
-                <template v-for="(item, index) in items">
-                  <v-list-item :key="item.title">
+                <template v-for="(grupo, index) in grupos">
+                  <v-list-item :key="grupo.title">
                     <template >
                       <v-list-item-content>
-                        <v-list-item-title v-text="item.title"></v-list-item-title>
+                        <v-list-item-title v-text="grupo.title"></v-list-item-title>
 
                         <v-list-item-subtitle
                           class="text--primary"
                         ></v-list-item-subtitle>
 
-                        <v-list-item-subtitle v-text="item.subtitle"></v-list-item-subtitle>
+                        <v-list-item-subtitle v-text="grupo.subtitle"></v-list-item-subtitle>
                       </v-list-item-content>
                     </template>
                   </v-list-item>
-
                   <v-divider
-                    v-if="index < items.length - 1"
+                    v-if="index < grupo.length - 1"
                     :key="index"
                   ></v-divider>
                 </template>
@@ -55,33 +55,34 @@
             </v-list>
           </v-card>
         </v-col>
+        <!-- Fim da Lista de Grupos -->
+        <!-- Inicio da Lista de Marcas -->
         <v-col
         cols="6"
         >
           <v-card
           color="purple"
           >
-            <v-card-title class="d-flex justify-center">Estoque Grupo</v-card-title>
+            <v-card-title class="d-flex justify-center">Estoque Marcas</v-card-title>
             <v-list two-line>
               <v-list-item-group
               >
-                <template v-for="(item, index) in items">
-                  <v-list-item :key="item.title">
-                    <template >
+                <template v-for="(marca, index) in marcas">
+                  <v-list-item :key="marca.title">
+                    <template>
                       <v-list-item-content>
-                        <v-list-item-title v-text="item.title"></v-list-item-title>
-
-                        <v-list-item-subtitle
-                          class="text--primary"
-                        ></v-list-item-subtitle>
-
-                        <v-list-item-subtitle v-text="item.subtitle"></v-list-item-subtitle>
+                        <v-list-item-title v-text="marca.title"></v-list-item-title>
                       </v-list-item-content>
+                      <v-list-content>
+                        <v-chip color="purple">
+                          <v-list-item-title v-text="marca.subtitle"></v-list-item-title>
+                        </v-chip>
+                      </v-list-content>
                     </template>
                   </v-list-item>
 
                   <v-divider
-                    v-if="index < items.length - 1"
+                    v-if="index < marca.length - 1"
                     :key="index"
                   ></v-divider>
                 </template>
@@ -89,6 +90,7 @@
             </v-list>
           </v-card>
         </v-col>
+        <!-- Fim da Lista de Marcas -->
       </v-row>
     </v-container>
   </v-card>
@@ -101,7 +103,29 @@
         { title: 'Aguardando Confirmação', flex: 4, color:'#ffd23f', num: 500},
         { title: 'Vendas Concluidas', flex: 4, color:'#119da4', num: 2150 },
       ],
-      items: [
+      grupos: [
+        {
+          subtitle: 'Esportiva',
+          title: 'Camisa',
+        },
+        {
+          subtitle: 'Corrida',
+          title: 'Tenis',
+        },
+        {
+          subtitle: 'Inverno',
+          title: 'Jaqueta',
+        },
+        {
+          subtitle: 'Verão',
+          title: 'Sandalia',
+        },
+        {
+          subtitle: 'Jeans',
+          title: 'Calça',
+        },
+      ],
+      marcas: [
         {
           subtitle: 'Esportiva',
           title: 'Camisa',
