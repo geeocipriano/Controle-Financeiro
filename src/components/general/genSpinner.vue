30f64e713afc893879388b31907660c0ef08bd88
Geovany Cipriano <geocipriano28@gmail.com>
2021-05-27 09:37:13 -0300

    Create genSpinner.vue

---
+++ src/components/general/genSpinner.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="text-center">
+    <v-progress-circular indeterminate color="red"></v-progress-circular>
+  </div>
+</template>
+<script>
+export default { name: "genSpinner" };
+</script>
+
+<style scoped>
+.v-progress-circular {
+  margin: 1rem;
+}
+</style>
