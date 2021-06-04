6494b55679288168306395f3686c9eee45d6dd42
Geovany Cipriano <geocipriano28@gmail.com>
2021-06-03 21:41:24 -0300

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,5 @@
 <template>
   <v-app id="inspire">
-    <side-bar />
-    <v-app-bar app color="#343a40" dark>
-      <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
-      <v-spacer></v-spacer>
-      <v-toolbar-title>CONTROLE GERENCIAL</v-toolbar-title>
-      <v-spacer></v-spacer>
-    </v-app-bar>
     <v-main>
       <gen-spinner />
       <router-view></router-view>
@@ -15,29 +8,9 @@
 </template>
 
 <script>
-import SideBar from "./components/bars/sideBar.vue";
 import genSpinner from "./components/general/genSpinner.vue";
 export default {
-  components: { genSpinner, SideBar },
-  data: () => ({
-    drawer: null,
-    items: [
-      { title: "Home", icon: "mdi-home", to: "/", color: "#e76f51" },
-      {
-        title: "Produtos",
-        icon: "mdi-basket",
-        to: "/Produtos",
-        color: "#e76f51",
-      },
-      {
-        title: "Financeiro",
-        icon: "mdi-cash",
-        to: "/Financeiro",
-        color: "#e76f51",
-      },
-    ],
-    right: null,
-  }),
+  components: { genSpinner },
   mounted() {
     console.log(this.$firebase);
   },
