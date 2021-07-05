47e96a92266ce9e34ddc38e9316c9c839951201d
Geovany Cipriano <geocipriano28@gmail.com>
2021-07-05 18:20:19 -0300

    Update Home.vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,147 +1,76 @@
 <template>
-  <v-card class="mx-auto" width="100vw" height="90vh">
-    <v-container fluid>
-      <v-row dense>
-        <v-col v-for="card in cards" :key="card.title" :cols="card.flex">
-          <v-card :color="card.color" class="px-6">
-            <v-card-title
-              v-text="card.title"
-              dark
-              class="mb-4 d-flex justify-center"
-            ></v-card-title>
-            <v-card-subtitle
-              dark
-              class="d-flex justify-center font-weight-bold mb-4"
-            >
-              <h1 v-text="card.num"></h1>
-            </v-card-subtitle>
-          </v-card>
-        </v-col>
-        <!-- Inicio Lista de Grupos -->
-        <v-col cols="6">
-          <v-card color="red">
-            <v-card-title class="d-flex justify-center"
-              >Estoque Grupo</v-card-title
-            >
-            <v-list two-line>
-              <v-list-item-group>
-                <template v-for="(grupo, index) in grupos">
-                  <v-list-item :key="grupo.title">
-                    <template>
-                      <v-list-item-content>
-                        <v-list-item-title
-                          v-text="grupo.title"
-                        ></v-list-item-title>
-                      </v-list-item-content>
-                      <v-list-content>
-                        <v-chip color="red" label>
-                          <v-list-item-title
-                            v-text="grupo.estoque"
-                          ></v-list-item-title>
-                        </v-chip>
-                      </v-list-content>
-                    </template>
-                  </v-list-item>
-                  <v-divider
-                    v-if="index < grupo.length - 1"
-                    :key="index"
-                  ></v-divider>
-                </template>
-              </v-list-item-group>
-            </v-list>
-          </v-card>
-        </v-col>
-        <!-- Fim da Lista de Grupos -->
-        <!-- Inicio da Lista de Marcas -->
-        <v-col cols="6">
-          <v-card color="purple">
-            <v-card-title class="d-flex justify-center"
-              >Estoque Marcas</v-card-title
-            >
-            <v-list two-line>
-              <v-list-item-group>
-                <template v-for="(marca, index) in marcas">
-                  <v-list-item :key="marca.title">
-                    <template>
-                      <v-list-item-content>
-                        <v-list-item-title
-                          v-text="marca.title"
-                        ></v-list-item-title>
-                      </v-list-item-content>
-                      <v-list-content>
-                        <v-chip color="purple" label>
-                          <v-list-item-title
-                            v-text="marca.estoque"
-                          ></v-list-item-title>
-                        </v-chip>
-                      </v-list-content>
-                    </template>
-                  </v-list-item>
-                  <v-divider
-                    v-if="index < marca.length - 1"
-                    :key="index"
-                  ></v-divider>
-                </template>
-              </v-list-item-group>
-            </v-list>
-          </v-card>
-        </v-col>
-        <!-- Fim da Lista de Marcas -->
-      </v-row>
-    </v-container>
-  </v-card>
+  <v-app id="inspire">
+    <v-col>
+      <v-navigation-drawer
+        v-model="drawer"
+        app
+        class="pt-4"
+        color="#343a40"
+        mini-variant
+      >
+        <v-list shaped class="my-10">
+          <v-list-item-group v-model="selectedItem" color="#e76f51">
+            <v-list-item v-for="(item, i) in items" :key="i">
+              <v-list-item-icon>
+                <v-icon v-text="item.icon"></v-icon>
+              </v-list-item-icon>
+              <v-list-item-content>
+                <v-list-item-title v-text="item.text"></v-list-item-title>
+              </v-list-item-content>
+            </v-list-item>
+          </v-list-item-group>
+        </v-list>
+      </v-navigation-drawer>
+    </v-col>
+    <v-col>
+      <v-app-bar absolute color="#343a40" dark>
+        <v-spacer></v-spacer>
+        <h1>Controle Financeiro</h1>
+
+        <v-spacer></v-spacer>
+        <v-btn icon class="mr-5">
+          <v-avatar
+            color="#e76f51"
+            class="d-flex text-center justify-center"
+          ></v-avatar>
+        </v-btn>
+      </v-app-bar>
+    </v-col>
+
+    <v-main class="grey lighten-3">
+      <v-container color="black">
+        <v-row>
+          <v-col cols="12" sm="2">
+            <v-sheet rounded="lg" min-height="268">
+              <!--  -->
+            </v-sheet>
+          </v-col>
+
+          <v-col cols="12" sm="8">
+            <v-sheet min-height="70vh" rounded="lg">
+              <!--  -->
+            </v-sheet>
+          </v-col>
+
+          <v-col cols="12" sm="2">
+            <v-sheet rounded="lg" min-height="268">
+              <!--  -->
+            </v-sheet>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-main>
+  </v-app>
 </template>
+
 <script>
 export default {
   data: () => ({
-    cards: [
-      { title: "Novas Vendas", flex: 4, color: "#beee62", num: 1250 },
-      { title: "Aguardando Confirmação", flex: 4, color: "#ffd23f", num: 500 },
-      { title: "Vendas Concluidas", flex: 4, color: "#119da4", num: 2150 },
-    ],
-    grupos: [
-      {
-        estoque: "5",
-        title: "Camisa",
-      },
-      {
-        estoque: "15",
-        title: "Tenis",
-      },
-      {
-        estoque: "10",
-        title: "Jaqueta",
-      },
-      {
-        estoque: "20",
-        title: "Sandalia",
-      },
-      {
-        estoque: "25",
-        title: "Calça",
-      },
-    ],
-    marcas: [
-      {
-        estoque: 13,
-        title: "Camisa",
-      },
-      {
-        estoque: 11,
-        title: "Tenis",
-      },
-      {
-        estoque: 15,
-        title: "Jaqueta",
-      },
-      {
-        estoque: 20,
-        title: "Sandalia",
-      },
-      {
-        estoque: 25,
-        title: "Calça",
-      },
+    selectedItem: 1,
+    items: [
+      { text: "Real-Time", icon: "mdi-clock" },
+      { text: "Audience", icon: "mdi-account" },
+      { text: "Conversions", icon: "mdi-flag" },
     ],
   }),
 };
